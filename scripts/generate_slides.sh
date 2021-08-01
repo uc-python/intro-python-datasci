f768b16d2d39aa7e9a4ed71116974c04bf9dbc3b
Ethan Swan <eswan18@rocketmail.com>
2021-08-01 16:31:13 -0400

    Update generate_slides script.
    Now with more precise pattern-matching to decide which notebooks should
    be turned into slides.

--- scripts/generate_slides.sh
+++ scripts/generate_slides.sh
@@ -1,4 +1,5 @@
 set -e
+shopt -s extglob
 
 if [ ! -d ".git" ]; then
     echo "Error: no .git directory detected"
@@ -11,8 +12,7 @@ fi
 # correctly..
 cd notebooks
 cp -r images ../slides
-# Match all notebook files with content.
-for file in *-*.ipynb; do
-    # jupyter nbconvert --to slides $file --reveal-prefix https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0 --output-dir=../slides
+# Match all notebook files starting with digits and then a dash.
+for file in +([0-9])*-*.ipynb; do
     jupyter nbconvert --to slides $file --output-dir=../slides
 done
