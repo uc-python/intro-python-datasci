514af590ab0c58bffa56c9fbd754d6e68aefb92f
Ethan Swan <eswan18@rocketmail.com>
2021-08-12 20:22:59 -0500

    Remove hide_code_in_slideshow; close #55.

--- scripts/generate_slides.sh
+++ scripts/generate_slides.sh
@@ -14,5 +14,6 @@ cd notebooks
 cp -r images ../slides
 # Match all notebook files starting with digits and then a dash.
 for file in +([0-9])*-*.ipynb; do
-    jupyter nbconvert --to slides $file --output-dir=../slides
+    jupyter nbconvert --to slides $file --TagRemovePreprocessor.remove_input_tags hide-input --output-dir=../slides
+    # jupyter nbconvert --to slides $file --TagRemovePreprocessor.remove_input_tags={\"hide-input\"} --TagRemovePreprocessor.remove_input_tags hide-input --output-dir=../slides
 done
